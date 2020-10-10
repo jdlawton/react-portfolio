ea9a043335243c712f95f8f58c6c6a77da241e37
JoeL <jlawton1203@gmail.com>
2020-10-09 21:07:36 -0500

    I added the Project component to list all of the projects I'm going to have on my portfolio, though they aren't fully styled yet, the data is all there and the links all point to the appropriate place. I also added the Resume component including a list of my fron-end and back-end proficiencies and a link to a downloadable pdf of my resume.

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,30 +1,66 @@
 import React from 'react';
 
+const projects = [
+    {
+        name: "Run Buddy",
+        liveURL: "https://jdlawton.github.io/run-buddy/",
+        ghURL: "https://github.com/jdlawton/run-buddy",
+        tech: "HTML/CSS",
+        style: "run-buddy"
+    },
+    {
+        name: "Robot Gladiators",
+        liveURL: "https://jdlawton.github.io/robot-gladiators/",
+        ghURL: "https://github.com/jdlawton/robot-gladiators",
+        tech: "JavaScript",
+        style: "robot-gladiators"
+    },
+    {
+        name: "Prestige Worldwide",
+        liveURL: "https://jdlawton.github.io/prestige-worldwide/",
+        ghURL: "https://github.com/jdlawton/prestige-worldwide",
+        tech: "HTML/CSS/JavaScript",
+        style: "prestige-worldwide"
+    },
+    {
+        name: "Fallout 4 Character Manager",
+        liveURL: "https://jdlawton.github.io/fo-char-gen",
+        ghURL: "https://github.com/jdlawton/fo4-char-gen",
+        tech: "Node.js/Express/Sequelize/Handlebars",
+        style: "fo-char-mgr"
+    },
+    {
+        name: "Noteworthy",
+        liveURL: "https://lit-shelf-32609.herokuapp.com/",
+        ghURL: "https://github.com/jdlawton/noteworthy",
+        tech: "Node.js/Express",
+        style: "noteworthy"
+    },
+    {
+        name: "/dev/zero > blog",
+        liveURL: "https://fast-caverns-43047.herokuapp.com/",
+        ghURL: "https://github.com/jdlawton/tech-blog",
+        tech: "Node.js/Express/MVC",
+        style: "blog"
+    }
+]
+
 //This component returns the div that contains all of the content for the Projects portion of the site.
 const Projects = () => (
     <div>
-        <h1>Projects Page</h1>
-        <p>
-            Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui mauris,
-            ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus porta. Nam
-            quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-            imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed rhoncus mollis
-            diam, sit amet facilisis lectus blandit at.
-        </p>
-        <p>
-            Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui mauris,
-            ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus porta. Nam
-            quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-            imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed rhoncus mollis
-            diam, sit amet facilisis lectus blandit at.
-        </p>
-        <p>
-            Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui mauris,
-            ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus porta. Nam
-            quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-            imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed rhoncus mollis
-            diam, sit amet facilisis lectus blandit at.
-        </p>
+        <h1>Projects</h1>
+            {projects.map(project => (
+                <div class={project.style}> 
+                    <a href={project.liveURL} target="_blank" rel="noopener noreferrer" key={project.name }>
+                        {project.name}
+                    </a>
+                    <a href={project.ghURL} target="_blank" rel="noopener noreferrer">
+                        GitHub Repo
+                    </a>
+                    <p>{project.tech}</p>
+                </div>
+                
+            ))}
     </div>
 );
 
